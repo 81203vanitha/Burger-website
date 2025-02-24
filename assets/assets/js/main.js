bd1121f2900fb78b4019cf7fb2b23b5b092800c9
81203vanitha <vanithasomasundharam4@gmail.com>
2025-02-24 11:16:14 +0530

    folder modified

--- assets/assets/js/main.js
+++ assets/assets/js/main.js
@@ -6,16 +6,18 @@ navClose=document.getElementById('nav-close')
 if(navToggle){
     navToggle.addEventListener('click',()=>{
         navMenu.classList.add('show-menu')
-    })
+    });
 }
 // menu hidden
 if(navClose){
     navClose.addEventListener('click',()=>{
         navMenu.classList.remove('show-menu')
-    })
+    });
 }
+
+
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink=document.querySelectorAll('.nav--link')
+const navLink=document.querySelectorAll('.nav__link')
 const linkAction=()=>{
     const navMenu=document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
